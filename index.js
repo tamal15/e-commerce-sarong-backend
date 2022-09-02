46e246c752232917fab0f0976fda15869945e82b
tamal15 <tamal15-13243@diu.edu.bd>
2022-09-02 12:20:51 +0600

    backend

--- index.js
+++ index.js
@@ -324,6 +324,13 @@ async function run() {
         const result=await userCollection.findOne(query)
         res.json(result)
     });
+      // user profile email 
+      app.get('/userupdateUser/:email', async(req,res)=>{
+        const email=req.params.email;
+        const query={email:email};
+        const result=await userCollection.findOne(query)
+        res.json(result)
+    });
 
          // post review the database 
     // app.post("/review", async (req, res) => {
